608d493efd4ab62ba454e1ced287efc3b88d297b
paulofor <paulofore@gmail.com>
2025-07-04 20:14:37 -0300

    Fix JPA repository scanning

--- backend/ads-service/src/main/java/com/marketinghub/ads/AdsServiceApplication.java
+++ backend/ads-service/src/main/java/com/marketinghub/ads/AdsServiceApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication(scanBasePackages = "com.marketinghub")
+@EnableJpaRepositories(basePackages = "com.marketinghub")
+@EntityScan(basePackages = "com.marketinghub")
 @EnableAsync
 @EnableScheduling
 public class AdsServiceApplication {

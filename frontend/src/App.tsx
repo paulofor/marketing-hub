898ee6ca1b6169e4fd798bdef092c3c6809a499e
paulofor <paulofore@gmail.com>
2025-07-03 21:22:02 -0300

    Improve frontend design with Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import { Route, Routes, Link } from "react-router-dom";
+
 import FacebookAccountsPage from "./pages/FacebookAccountsPage";
 import InstagramAccountsPage from "./pages/InstagramAccountsPage";
 
 export default function App() {
   return (
-    <div className="p-4">
-      <nav>
-        <Link to="/accounts/facebook">Facebook Accounts</Link> |{" "}
-        <Link to="/accounts/instagram">Instagram Accounts</Link>
+    <div className="container py-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Marketing Hub
+          </Link>
+          <div className="navbar-nav">
+            <Link className="nav-link" to="/accounts/facebook">
+              Facebook Accounts
+            </Link>
+            <Link className="nav-link" to="/accounts/instagram">
+              Instagram Accounts
+            </Link>
+          </div>
+        </div>
       </nav>
       <Routes>
         <Route path="/accounts/facebook" element={<FacebookAccountsPage />} />

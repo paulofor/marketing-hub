362cb6ccd3b949397d6c5dca0e2804c6a4388e61
paulofor <paulofore@gmail.com>
2025-07-04 13:50:58 -0300

    test: replace MySQL container with H2

--- backend/ads-service/src/test/java/com/marketinghub/media/AssetRepositoryTest.java
+++ backend/ads-service/src/test/java/com/marketinghub/media/AssetRepositoryTest.java
@@ -4,26 +4,11 @@ import com.marketinghub.media.repository.AssetRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
 @DataJpaTest
 class AssetRepositoryTest {
-    @Container
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
-
-    @DynamicPropertySource
-    static void mysqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
 
     @Autowired
     AssetRepository repository;

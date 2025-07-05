f7b435a6a1737addd71906db707a92f7ab8bee7b
paulofor <paulofore@gmail.com>
2025-07-04 21:44:38 -0300

    Enable JPA scanning

--- backend/ads-service/src/main/java/com/marketinghub/ads/AdsServiceApplication.java
+++ backend/ads-service/src/main/java/com/marketinghub/ads/AdsServiceApplication.java
@@ -2,10 +2,14 @@ package com.marketinghub.ads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.marketinghub")
+@EntityScan(basePackages = "com.marketinghub")
+@EnableJpaRepositories(basePackages = "com.marketinghub")
 @EnableAsync
 @EnableScheduling
 public class AdsServiceApplication {

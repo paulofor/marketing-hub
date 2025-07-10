ffe33701aebd7ed08d923f77fd543a2814ebb39f
Paulo Forestieri <paulofore@gmail.com>
2025-07-09 21:45:01 -0300

    acessando backend remoto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       // Backend now listens on port 8000
-      "/api": "http://localhost:8000",
+      "/api": "http://191.252.92.222:8000",
     },
   },
   test: {

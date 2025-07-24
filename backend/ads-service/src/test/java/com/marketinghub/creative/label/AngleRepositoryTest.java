424b6aede6dbabb0b0b072c480aba15002b8e035
paulofor <paulofore@gmail.com>
2025-07-24 14:32:50 -0300

    test: disable liquibase

--- backend/ads-service/src/test/java/com/marketinghub/creative/label/AngleRepositoryTest.java
+++ backend/ads-service/src/test/java/com/marketinghub/creative/label/AngleRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ContextConfiguration(classes = AdsServiceApplication.class)
+@TestPropertySource(properties = "spring.liquibase.enabled=false")
 class AngleRepositoryTest {
 
     @Autowired
--- backend/ads-service/src/test/java/com/marketinghub/experiment/ExperimentServiceTest.java
+++ backend/ads-service/src/test/java/com/marketinghub/experiment/ExperimentServiceTest.java
@@ -21,7 +21,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
         "spring.datasource.url=jdbc:h2:mem:testdb",
         "spring.datasource.driverClassName=org.h2.Driver",
         "spring.datasource.username=sa",
-        "spring.jpa.hibernate.ddl-auto=create"
+        "spring.jpa.hibernate.ddl-auto=create",
+        "spring.liquibase.enabled=false"
 })
 class ExperimentServiceTest {
     @Autowired

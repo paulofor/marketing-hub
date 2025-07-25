66a9e645278b918fedb1f29e1230a94a0efb0d54
paulofor <paulofore@gmail.com>
2025-07-25 07:48:36 -0300

    fix liquibase sql scripts

--- backend/ads-service/src/main/resources/db/changelog/V2025_07_01__add_fk_niche_to_hypothesis.sql
+++ backend/ads-service/src/main/resources/db/changelog/V2025_07_01__add_fk_niche_to_hypothesis.sql
@@ -1,3 +1,5 @@
+-- liquibase formatted sql
+-- changeset marketinghub:2025-07-01-add-fk-niche-to-hypothesis
 ALTER TABLE hypothesis
     MODIFY market_niche_id BIGINT NOT NULL;
 ALTER TABLE hypothesis

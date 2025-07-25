ed4ea3638f2145310dff9ff3bedbd7746426ac59
Paulo Forestieri <paulofore@gmail.com>
2025-07-25 15:02:09 -0300

    chave

--- backend/ads-service/src/main/resources/db/changelog/V2025_07_24__add_fk_hypothesis_to_experiment.sql
+++ backend/ads-service/src/main/resources/db/changelog/V2025_07_24__add_fk_hypothesis_to_experiment.sql
@@ -1,9 +1,14 @@
 -- liquibase formatted sql
 -- changeset marketinghub:2025-07-24-add-fk-hypothesis-to-experiment
--- preconditions onFail:MARK_RAN
---    <not>
---        <columnExists tableName="experiment" columnName="hypothesis_id"/>
---    </not>
+-- preconditions onFail:MARK_RAN onError:HALT
+--   sql-check expectedResult:0 SELECT COUNT(*) 
+--     FROM INFORMATION_SCHEMA.COLUMNS
+--     WHERE TABLE_SCHEMA='marketinghubdb'
+--       AND TABLE_NAME='experiment'
+--       AND COLUMN_NAME='hypothesis_id';
+
 ALTER TABLE experiment ADD COLUMN hypothesis_id BINARY(16) NOT NULL;
+
 ALTER TABLE experiment
-    ADD CONSTRAINT fk_experiment_hypothesis FOREIGN KEY (hypothesis_id) REFERENCES hypothesis(id);
+    ADD CONSTRAINT fk_experiment_hypothesis
+    FOREIGN KEY (hypothesis_id) REFERENCES hypothesis(id);

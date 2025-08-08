01ed277d7842ecc16af1c6bc5b6179784ec02fb0
paulofor <paulofore@gmail.com>
2025-08-08 17:09:55 -0300

    test: isolate FunnelController web slice

--- backend/ads-service/src/test/java/com/marketinghub/funnel/FunnelControllerTest.java
+++ backend/ads-service/src/test/java/com/marketinghub/funnel/FunnelControllerTest.java
@@ -2,9 +2,9 @@ package com.marketinghub.funnel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -15,11 +15,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.marketinghub.ads.AdsServiceApplication;
-
 @WebMvcTest(FunnelController.class)
-@ContextConfiguration(classes = AdsServiceApplication.class)
 class FunnelControllerTest {
+
+    @SpringBootApplication
+    static class TestConfig {}
 
     @Autowired
     private MockMvc mockMvc;

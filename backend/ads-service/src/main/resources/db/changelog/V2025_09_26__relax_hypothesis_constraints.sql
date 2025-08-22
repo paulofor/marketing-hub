a496ec445a33d453395c4f1e467fef72e77ff70e
paulofor <paulofore@gmail.com>
2025-08-21 17:44:28 -0700

    Add Liquibase formatted SQL header to relax hypothesis constraints

--- backend/ads-service/src/main/resources/db/changelog/V2025_09_26__relax_hypothesis_constraints.sql
+++ backend/ads-service/src/main/resources/db/changelog/V2025_09_26__relax_hypothesis_constraints.sql
@@ -1,3 +1,4 @@
+-- liquibase formatted sql
 -- changeset marketinghub:2025-09-26-relax-hypothesis-constraints
 ALTER TABLE hypothesis MODIFY kpi_target_cpl DECIMAL(7,2) NULL;
 ALTER TABLE hypothesis MODIFY offer_type VARCHAR(20) NULL;

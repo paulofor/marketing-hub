b0ad5f8b4e01fd01b905552801fe13301dbc521c
paulofor <paulofore@gmail.com>
2025-09-05 13:22:48 -0700

    feat: schedule instagram campaigns

--- facebook-ads-worker/src/main/java/com/marketinghub/facebookadsworker/FacebookAdsWorkerApplication.java
+++ facebook-ads-worker/src/main/java/com/marketinghub/facebookadsworker/FacebookAdsWorkerApplication.java
@@ -2,8 +2,10 @@ package com.marketinghub.facebookadsworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FacebookAdsWorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(FacebookAdsWorkerApplication.class, args);

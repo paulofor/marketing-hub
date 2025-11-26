6ca606106b8837997ed9daf65e05add7897ce0c9
paulofor <paulofore@gmail.com>
2025-11-25 21:42:00 -0300

    Revert "Add pending image packages view and API"

--- lead-portal/backend/src/main/java/com/marketinghub/leadportal/repository/FlowSubmissionImagePackageRepository.java
+++ lead-portal/backend/src/main/java/com/marketinghub/leadportal/repository/FlowSubmissionImagePackageRepository.java
@@ -1,11 +1,7 @@
 package com.marketinghub.leadportal.repository;
 
 import com.marketinghub.leadportal.entity.FlowSubmissionImagePackageEntity;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlowSubmissionImagePackageRepository
-        extends JpaRepository<FlowSubmissionImagePackageEntity, Long> {
-
-    List<FlowSubmissionImagePackageEntity> findByStatusIn(List<String> statuses);
-}
+        extends JpaRepository<FlowSubmissionImagePackageEntity, Long> {}

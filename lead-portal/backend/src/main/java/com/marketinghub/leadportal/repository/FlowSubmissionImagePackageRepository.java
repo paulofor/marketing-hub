1053af487b79f17017c37b6c508aa699f056dfaa
paulofor <paulofore@gmail.com>
2025-11-25 16:13:20 -0800

    Register lead portal uploads for image pipeline

---
+++ lead-portal/backend/src/main/java/com/marketinghub/leadportal/repository/FlowSubmissionImagePackageRepository.java
@@ -0,0 +1,7 @@
+package com.marketinghub.leadportal.repository;
+
+import com.marketinghub.leadportal.entity.FlowSubmissionImagePackageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlowSubmissionImagePackageRepository
+        extends JpaRepository<FlowSubmissionImagePackageEntity, Long> {}

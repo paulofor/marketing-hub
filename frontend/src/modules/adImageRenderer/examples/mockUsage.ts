af0bd265b31fc9a444738355d446514faebf4dda
paulofor <paulofore@gmail.com>
2026-04-03 12:05:35 -0300

    Revert "AI Hub: Resumo - Criei o módulo `frontend/src/modules/adImageRenderer` com a função principal `renderAdImagePayloads`, tipos públicos, validações e todo o conjunto de funções auxiliares exigido (escolha de placement, casamento copy/briefing, normalização …"

--- frontend/src/modules/adImageRenderer/examples/mockUsage.ts
+++
@@ -1,7 +0,0 @@
-import { createMockRenderAdImageInput } from "../mockData";
-import { RenderAdImagePayloadResult, renderAdImagePayloads } from "../renderAdImagePayloads";
-
-export function buildMockImageRenderExample(): RenderAdImagePayloadResult {
-  const input = createMockRenderAdImageInput();
-  return renderAdImagePayloads(input);
-}

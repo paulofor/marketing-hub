ee2b627f19373f42548d35b5c30cef7dc3994a16
paulofor <paulofore@gmail.com>
2026-05-27 19:08:22 -0300

    refactor(ai-worker): mover executor raiz do GeraLanding para pacote comum

--- ai-worker/src/main/java/com/marketinghub/worker/geralanding/GeraLandingExecutionScheduler.java
+++ ai-worker/src/main/java/com/marketinghub/worker/geralanding/GeraLandingExecutionScheduler.java
@@ -1,5 +1,6 @@
 package com.marketinghub.worker.geralanding;
 
+import com.marketinghub.worker.geralanding.comum.GeraLandingExecutionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;

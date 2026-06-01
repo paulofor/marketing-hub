df064e92d855d289ff83c8976b226ed6cfd7df65
paulofor <paulofore@gmail.com>
2026-06-01 19:03:30 -0300

    Agenda ciclo Hotmart para 21h

--- mois-hotmart-collector/src/test/java/com/marketinghub/moishotmart/service/HotmartCollectorSchedulerTest.java
+++ mois-hotmart-collector/src/test/java/com/marketinghub/moishotmart/service/HotmartCollectorSchedulerTest.java
@@ -12,15 +12,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 class HotmartCollectorSchedulerTest {
 
     /**
-     * Garante que o ciclo 1 esteja agendado para 18:15 de 1 de junho de 2026 no fuso de São Paulo.
+     * Garante que o ciclo 1 esteja agendado para 21:00 de 1 de junho de 2026 no fuso de São Paulo.
      */
     @Test
-    void shouldScheduleFirstCycleAtEighteenFifteenOnJuneFirst() throws NoSuchMethodException {
+    void shouldScheduleFirstCycleAtTwentyOneOnJuneFirst() throws NoSuchMethodException {
         Method method = HotmartCollectorScheduler.class
-                .getDeclaredMethod("collectFirstCycleAtEighteenFifteenOnJuneFirst2026");
+                .getDeclaredMethod("collectFirstCycleAtTwentyOneOnJuneFirst2026");
         Scheduled scheduled = method.getAnnotation(Scheduled.class);
 
-        assertEquals("0 15 18 1 6 *", scheduled.cron());
+        assertEquals("0 0 21 1 6 *", scheduled.cron());
         assertEquals("America/Sao_Paulo", scheduled.zone());
     }
 }
